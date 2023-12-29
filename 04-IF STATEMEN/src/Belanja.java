30e15c4da973b087a0bfcf4f739709a69fc5ee15
Ayusarass21 <146680663+Ayusarass21@users.noreply.github.com>
2023-12-29 11:14:14 +0800

    jumat, 29 Desember2023

---
+++ 04-IF STATEMEN/src/Belanja.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Belanja {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+      
+       // Data Member atau variabel
+       int tot_belanja;
+
+       // Membuat Objek scanner
+       Scanner input = new Scanner(System.in);
+       System.out.println("Masukan Total Belanja");
+
+       tot_belanja = input.nextInt();
+
+       // Membuat percabangan
+       if (tot_belanja >= 50000){
+            System.out.println("Selamat Dapat MOUSE");
+              }  else {
+                 System.out.println("Tidak Dapat MOUSE");
+
+
+
+              }
+            }
+        }
+
+            
+        
+
+
